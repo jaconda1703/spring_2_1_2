1e12bafb2de701ac1129a3399cb408918acf33ac
Jaconda <jaconda007@rambler.ru>
2026-01-28 17:14:19 +0300

    Task 2.1.2 full implementation

---
+++ src/main/java/Cat.java
@@ -0,0 +1,12 @@
+public class Cat {
+
+    private String breed;
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+}
